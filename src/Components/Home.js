82c87b193beed36e218a0d269cf6fa91e3870c89
19521679 <19521679@gm.uit.edu.vn>
2022-04-27 11:40:23 +0700

    update switch language frontend

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,38 +10,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div>
-      {/* ======= Hero Section ======= */}
-      <section id="hero" className="d-flex align-items-center">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-              <h1 data-aos="fade-up">Grow your business with Vesperr</h1>
-              <h2 data-aos="fade-up" data-aos-delay={400}>
-                We are team of talented designers making websites with Bootstrap
-              </h2>
-              <div data-aos="fade-up" data-aos-delay={800}>
-                <a href="#about" className="btn-get-started scrollto">
-                  Get Starteddd
-                </a>
-              </div>
-            </div>
-            <div
-              className="col-lg-6 order-1 order-lg-2 hero-img"
-              data-aos="fade-left"
-              data-aos-delay={200}
-            >
-              <img
-                src="assets/img/hero-img.png"
-                className="img-fluid animated"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* End Hero */}
-
-      <main id="main">
+      <section id="main">
         {/* ======= Clients Section ======= */}
         <section id="clients" className="clients clients">
           <div className="container">
@@ -3450,7 +3419,7 @@ export default function Home() {
           </div>
         </section>
         {/* End F.A.Q Section */}
-      </main>
+      </section>
       {/* End #main */}
     </div>
   );
